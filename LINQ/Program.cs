42d549f679a4aaae10bba2219123ad013fc33b9f
Daniel Finch <xiphoid0311@gmail.com>
2023-08-05 13:34:07 -0500

    list of video games ordered by LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<string> videoGames = new List<string>() { "Rainbow 6 Siege", "DarkSouls", "Castlevania"}    ;
+            videoGames.Add("Minecraft");
+            videoGames.Add("Legends of Dragoon");
+            videoGames.Add("Suikoden");
+            videoGames.Add("Ark");
+            videoGames.Add("Final Fantasy X");
+
+            var orderedGames = videoGames.OrderBy(name => name.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
